ef477e3235ac6ae31461165fea4e24fb3d9e2151
triggermortis <sam.smags@gmail.com>
2015-02-21 20:18:57 +0000

    7

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 #  get 'welcome/index'
 
   devise_for :users
-  root to: 'home#index'
+  root 'home#index'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
