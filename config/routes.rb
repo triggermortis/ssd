f1f1f3bae29fec54f79d1ac3d8c73f2fc90ad479
triggermortis <sam.smags@gmail.com>
2015-02-21 20:14:50 +0000

    7

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,8 @@ Rails.application.routes.draw do
 #  get 'welcome/index'
 
   devise_for :users
-
-  devise_scope :user do
-    authenticated :user do
-      root 'home#index', as: :authenticated_root
-    end
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end  
+  root to: 'home#index'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
